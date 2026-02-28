c23abc68ceaf9a84233ef5ec556a6fe39426ed19
james <J.Joseph.Taylor@obyliskconsulting.com>
2026-02-27 21:57:44 -0500

    More work on variables.

--- vars.py
+++ vars.py
@@ -6,3 +6,48 @@ sum_total = age + score
 print(f"Age: {age}")
 print(f"Score: {score}")
 print(f"Sum: {sum_total}")
+
+>>> is_student=True
+>>> is_student
+True
+>>> has_passed_exam= False
+>>> has_passed_exam
+False
+>>> result = 10 > 5
+>>> result
+True
+>>> is_equal = (15 == 15)
+>>> is_equal
+True
+>>> price = 19.99
+>>> price
+19.99
+>>> temperature = -2.5
+>>> temperature
+-2.5
+>>> result = 10.5 + 3.7
+>>> result
+14.2
+>>> division = 10.0 / 3.0
+>>> division
+3.3333333333333335
+>>> name = John
+Traceback (most recent call last):
+  File "<python-input-16>", line 1, in <module>
+    name = John
+           ^^^^
+NameError: name 'John' is not defined
+>>> name = "John"
+>>> name
+'John'
+>>> greeting = "Hello World"
+>>> gretting
+Traceback (most recent call last):
+  File "<python-input-20>", line 1, in <module>
+    gretting
+NameError: name 'gretting' is not defined. Did you mean: 'greeting'?
+>>> greeting
+'Hello World'
+>>> formatted = f"{name} says {greeting}"
+>>> formatted
+'John says Hello World
